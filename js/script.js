896e556fa4d218f7bf8b0ea11ba6712745e8e93f
Claudio-Emmolo <claudioemmolo90@gmail.com>
2022-11-18 14:59:52 +0100

    Add Js - innerhtml for print

--- js/script.js
+++ js/script.js
@@ -3,3 +3,5 @@ let namePeople = prompt ("Qual'è il tuo nome?"); //Chiedo il nome
 let surnamePeople = prompt ("Qual'è il tuo cognome?"); //Chiedo il cognome
 
 let colorLove = prompt ("Qual'è il tuo colore preferito?"); //Chiedo il colore preferito
+
+document.getElementById("name") .innerHTML = namePeople + " " + surnamePeople + " " + colorLove + " " + "22";
