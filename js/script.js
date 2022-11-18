03bde5b409cd206f7050e7f871323fb8facc40b8
Claudio-Emmolo <claudioemmolo90@gmail.com>
2022-11-18 15:10:16 +0100

    Add Console Print

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 let namePeople = prompt ("Qual'è il tuo nome?"); //Chiedo il nome
+console.log (namePeople); //Console Print
 
 let surnamePeople = prompt ("Qual'è il tuo cognome?"); //Chiedo il cognome
+console.log (surnamePeople); //Console Print
 
 let colorLove = prompt ("Qual'è il tuo colore preferito?"); //Chiedo il colore preferito
+console.log (colorLove); //Console Print
 
 document.getElementById("name") .innerHTML = namePeople + " " + surnamePeople + " " + colorLove + " " + "22";
+
