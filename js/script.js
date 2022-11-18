9819466e2c59f6ec5148ee51496003d7988f7c75
Claudio-Emmolo <claudioemmolo90@gmail.com>
2022-11-18 14:56:50 +0100

    Add request for name, surname and color

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let namePeople = prompt ("Qual'è il tuo nome?"); //Chiedo il nome
+
+let surnamePeople = prompt ("Qual'è il tuo cognome?"); //Chiedo il cognome
+
+let colorLove = prompt ("Qual'è il tuo colore preferito?"); //Chiedo il colore preferito
